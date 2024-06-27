8317b3ab5ed45699173d5b092ec651edf67fdf42
Darkzero7 <chaiyanituporn@gmail.com>
2024-06-27 23:41:54 +0700

    js lab 153

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-// write code here
+// จงเขียนฟังก์ชัน cloneArray โดยรับพารามิเตอร์มาเป็น Array และ return ค่าเป็น Array ที่มี element เหมือนกับพารามิเตอร์ที่รับเข้ามาทุกตัว
+
+function cloneArray(array) {
+    return [...array]
+}
+const originalArray = [1, 2, 3, 4, 5];
+const clonedArray = cloneArray(originalArray);
+
+console.log(originalArray); // [1, 2, 3, 4, 5]
+console.log(clonedArray); // [1, 2, 3, 4, 5]
